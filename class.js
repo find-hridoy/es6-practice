e34878757cc23766e4c7b45dc1df865f20ca8dbf
hridoy546 <mdhridoymia546@gmail.com>
2020-08-08 21:42:34 +0600

    Added Class File

---
+++ class.js
@@ -0,0 +1,12 @@
+class Student {
+    constructor(studentName, studentRoll) {
+        this.name = studentName;
+        this.roll = studentRoll;
+        this.school = "Birabo Shorkari Prathomic Biddaloy";
+    }
+}
+const student1 = new Student("Rajib", 26);
+const student2 = new Student("Mridul", 4);
+const student3 = new Student("Al-Amin", 17);
+const student4 = new Student("Akash", 19);
+console.log(student1, student2, student3, student4);
