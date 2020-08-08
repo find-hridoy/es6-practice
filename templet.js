6c21d3ebf86149020d378c5e6752b151d9e60055
hridoy546 <mdhridoymia546@gmail.com>
2020-08-08 14:16:28 +0600

    templet file added

---
+++ templet.js
@@ -0,0 +1,9 @@
+const firstName = "Justin";
+const lastName = "Trodo";
+const fullName = firstName + " " + lastName + " is a Prime Minister";
+const fullName2 = `${firstName} ${lastName} is a Prime Minister`;
+const shortLine = `Ondhokare poth hariye dube gece jei tara,
+Take ami sob boleci, tomar golpo cara....`
+// console.log(fullName);
+// console.log(fullName2);
+console.log(shortLine);
