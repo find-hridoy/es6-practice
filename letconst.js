a780bf551c3109b1e56069f6bc483001b3302de2
hridoy546 <mdhridoymia546@gmail.com>
2020-08-08 13:55:07 +0600

    letconst and default js file added

---
+++ letconst.js
@@ -0,0 +1,11 @@
+const name = "Hridoy";
+// name = "Saber" // Not change
+// console.log(name);
+const names = ["Hey", "Yeah"];
+names.push = "Whats up?"
+// console.log(names); 
+
+
+let myName = "Hridoy";
+myName = "Saber Hossain Hridoy";
+console.log(myName);
